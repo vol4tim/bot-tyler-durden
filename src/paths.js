adab75f8fee812bbc4ecaa36eaf9676f4ac2ddc4
vol4tim <vol4tim@gmail.com>
2024-11-10 10:10:12 +0400

    add queue

--- src/paths.js
+++ src/paths.js
@@ -2,5 +2,6 @@ import path from "path";
 
 export const PATH_DB = path.join(__dirname, "/../files/database.sqlite");
 export const PATH_SESSION_DB = path.join(__dirname, "/../files/session.sqlite");
+export const PATH_QUEUE_DB = path.join(__dirname, "/../files/queue.sqlite");
 export const PATH_COURSE_CACHE = path.join(__dirname, "/../files/courses");
 export const PATH_LOGS = path.join(__dirname, "/../files/logs");
--- src/watcher.js
+++ src/watcher.js
@@ -1,12 +1,74 @@
+import Queue from "better-queue";
+import SqliteStore from "better-queue-sqlite";
 import { Markup } from "telegraf";
 import bot from "./bot";
 import Location, { STATUS } from "./models/location";
 import Profile from "./models/profile";
+import { PATH_QUEUE_DB } from "./paths";
 import { getApi, on } from "./tools/polkadot";
 import { t } from "./translate";
 
 export const watcher = async () => {
+  const store = new SqliteStore({ path: PATH_QUEUE_DB });
   const api = await getApi(process.env.ENDPOINT_PARACHAIN);
+
+  const queue = new Queue(
+    async function (r, cb) {
+      for (const item of r) {
+        if (item.cid.split("next_location").length >= 2) {
+          let location;
+          if (item.controller === process.env.CONTROLLER_1) {
+            location = 1;
+          } else if (item.controller === process.env.CONTROLLER_2) {
+            location = 2;
+          }
+          if (location) {
+            const profile = await Profile.findOne({
+              where: { location: location },
+            });
+            if (profile) {
+              const header = await api.rpc.chain.getHeader(item.createdAtHash);
+              const blockNumber = header.number.unwrap();
+              try {
+                if (location === 1) {
+                  await bot.telegram.sendMessage(
+                    profile.userId,
+                    t(profile.lang)
+                      .scene3.finish.replace("__blockNumber__", blockNumber)
+                      .replace("__txIndex__", item.txIndex),
+                    Markup.inlineKeyboard([
+                      Markup.button.callback("Next scene", "next-scene-3"),
+                    ]),
+                  );
+                } else if (location === 2) {
+                  await bot.telegram.sendMessage(
+                    profile.userId,
+                    t(profile.lang)
+                      .scene4.finish.replace("__blockNumber__", blockNumber)
+                      .replace("__txIndex__", item.txIndex),
+                    Markup.inlineKeyboard([
+                      Markup.button.callback("Next scene", "next-scene-44"),
+                    ]),
+                  );
+                }
+              } catch (error) {
+                console.log(error);
+              }
+              profile.location = null;
+              await profile.save();
+              await Location.update(
+                { status: STATUS.AVAIBLE },
+                { where: { number: location } },
+              );
+            }
+          }
+        }
+      }
+
+      cb(null, true);
+    },
+    { store: store },
+  );
 
   on(api, { section: "datalog", method: "NewRecord" }, async (results) => {
     const r = results
@@ -20,61 +82,12 @@ export const watcher = async () => {
       })
       .map((item) => {
         return {
-          createdAtHash: item.createdAtHash,
+          createdAtHash: item.createdAtHash.toHex(),
           txIndex: item.phase,
           controller: item.data[0].toHuman(),
           cid: item.data[2].toHuman(),
         };
       });
-    for (const item of r) {
-      if (item.cid.split("next_location").length >= 2) {
-        let location;
-        if (item.controller === process.env.CONTROLLER_1) {
-          location = 1;
-        } else if (item.controller === process.env.CONTROLLER_2) {
-          location = 2;
-        }
-        if (location) {
-          const profile = await Profile.findOne({
-            where: { location: location },
-          });
-          if (profile) {
-            const header = await api.rpc.chain.getHeader(item.createdAtHash);
-            const blockNumber = header.number.unwrap();
-            try {
-              if (location === 1) {
-                await bot.telegram.sendMessage(
-                  profile.userId,
-                  t(profile.lang)
-                    .scene3.finish.replace("__blockNumber__", blockNumber)
-                    .replace("__txIndex__", item.txIndex),
-                  Markup.inlineKeyboard([
-                    Markup.button.callback("Next scene", "next-scene-3"),
-                  ]),
-                );
-              } else if (location === 2) {
-                await bot.telegram.sendMessage(
-                  profile.userId,
-                  t(profile.lang)
-                    .scene4.finish.replace("__blockNumber__", blockNumber)
-                    .replace("__txIndex__", item.txIndex),
-                  Markup.inlineKeyboard([
-                    Markup.button.callback("Next scene", "next-scene-44"),
-                  ]),
-                );
-              }
-            } catch (error) {
-              console.log(error);
-            }
-            profile.location = null;
-            await profile.save();
-            await Location.update(
-              { status: STATUS.AVAIBLE },
-              { where: { number: location } },
-            );
-          }
-        }
-      }
-    }
+    queue.push(r);
   });
 };
